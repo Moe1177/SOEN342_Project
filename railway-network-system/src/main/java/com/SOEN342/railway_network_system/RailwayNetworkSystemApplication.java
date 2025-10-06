d242e393844b0b1642cfc4aca8035d8a174bc53d
enyc24 <1.emily.anne@gmail.com>
2025-10-06 14:28:10 -0400

    Created the spring project

---
+++ railway-network-system/src/main/java/com/SOEN342/railway_network_system/RailwayNetworkSystemApplication.java
@@ -0,0 +1,13 @@
+package com.SOEN342.railway_network_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayNetworkSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayNetworkSystemApplication.class, args);
+	}
+
+}
---
+++ railway-network-system/src/test/java/com/SOEN342/railway_network_system/RailwayNetworkSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SOEN342.railway_network_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayNetworkSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
