8bdb4d5944c050f70098649b3a40c86c4955068f
mijanullah12 <mijan.ullah12@gmail.com>
2025-10-10 17:20:51 -0400

    implemented database classes

--- railway-network-system/src/main/java/com/SOEN342/railway_network_system/database/TrainsDB.java
+++ railway-network-system/src/main/java/com/SOEN342/railway_network_system/database/TrainsDB.java
@@ -1,23 +1,28 @@
 package com.SOEN342.railway_network_system.database;
 
 import java.util.*;
+import org.springframework.stereotype.Component;
+
 import com.SOEN342.railway_network_system.model.Train;
 
+@Component
 public class TrainsDB {
-    private List<Train> trains = new ArrayList<>();
+    private final Map<String, Train> routeIdToTrain = new HashMap<>();
 
-    //add train to train database
-    public void addTrain(Train train){
-        
+    //add train to train database and associate with a routeId
+    public void addTrain(String routeID, Train train){
+        if (routeID == null || train == null) return;
+        routeIdToTrain.put(routeID, train);
     }
 
     //find train for specific route 
     public Train getTrainByRoute(String routeID){
-        return new Train(null, null);
+        if (routeID == null) return null;
+        return routeIdToTrain.get(routeID);
     }
 
     //returns all existing trains from database
     public List<Train> getAllTrains(){
-        return trains;
+        return new ArrayList<>(routeIdToTrain.values());
     }
 }
