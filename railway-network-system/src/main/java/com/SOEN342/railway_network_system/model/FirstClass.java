723ea5a1ec3b6aecb7b8d744fe0d6b73fe20ee92
enyc24 <1.emily.anne@gmail.com>
2025-10-23 22:17:42 -0400

    Updated ticket interface with new attribute ticketId

--- railway-network-system/src/main/java/com/SOEN342/railway_network_system/model/FirstClass.java
+++ railway-network-system/src/main/java/com/SOEN342/railway_network_system/model/FirstClass.java
@@ -1,5 +1,7 @@
 package com.SOEN342.railway_network_system.model;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class FirstClass implements Ticket {
     private double ticketRate;
+    private String ticketID;
 
     @Override
     public double getTicketRate(){
         return ticketRate;
+    }
+
+    @Override
+    public String getTicketId() {
+        ticketID = "TKT-" + UUID.randomUUID().toString().substring(0, 8);
+        return ticketID;
     }
 
     @Override
--- railway-network-system/src/main/java/com/SOEN342/railway_network_system/model/SecondClass.java
+++ railway-network-system/src/main/java/com/SOEN342/railway_network_system/model/SecondClass.java
@@ -1,5 +1,7 @@
 package com.SOEN342.railway_network_system.model;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SecondClass implements Ticket{
     private double ticketRate;
+    private String ticketId;
 
     @Override
     public double getTicketRate(){
         return ticketRate;
+    }
+
+    @Override 
+    public String getTicketId(){
+        ticketId = "TKT-" + UUID.randomUUID().toString().substring(0, 8);
+        return ticketId;
     }
 
     @Override
