dd1924195d18384b42d60c6da71cbb3cc0429729
enyc24 <1.emily.anne@gmail.com>
2025-11-06 23:42:32 -0500

    Create trains database

--- railway-network-system/src/main/java/com/SOEN342/railway_network_system/database/TrainsDB.java
+++ railway-network-system/src/main/java/com/SOEN342/railway_network_system/database/TrainsDB.java
@@ -1,6 +1,8 @@
 package com.SOEN342.railway_network_system.database;
 
 import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.SOEN342.railway_network_system.model.Train;
@@ -8,11 +10,21 @@ import com.SOEN342.railway_network_system.model.Train;
 @Component
 public class TrainsDB {
     private final Map<String, Train> routeIdToTrain = new HashMap<>();
+     private final TrainRepository trainRepository;
+
+    @Autowired
+    public TrainsDB(TrainRepository trainRepository) {
+        this.trainRepository = trainRepository;
+    }
 
     //add train to train database and associate with a routeId
     public void addTrain(String routeID, Train train){
         if (routeID == null || train == null) return;
         routeIdToTrain.put(routeID, train);
+        if (trainRepository != null) {
+            train.setRouteId(routeID);
+            trainRepository.save(train);
+        }
     }
 
     //find train for specific route 
